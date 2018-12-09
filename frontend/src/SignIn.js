bcf00dcfb672853fdc758790096506e14d1d21f9
Parth Chokshi <parthchokshi1@gmail.com>
2018-12-09 00:30:57 -0500

    signin changes

--- frontend/src/SignIn.js
+++ frontend/src/SignIn.js
@@ -1,13 +1,52 @@
-import React, { Component } from 'react';
-import './css/main.css';
+import React, { Component } from "react";
+import "./css/main.css";
 
 class Personal extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      username: "",
+      password: "",
+      showPopup: false,
+      loggedIn: false
+    };
+  }
+  toggleSignInPopup = () => {
+    this.setState({ showPopup: !this.state.showPopup });
+  };
+  onSubmit = () => {
+    this.setState({ loggedIn: true });
+  };
   render() {
     return (
       <>
-        <div className='signin-link'>
-            <p className='sign-in-text'> Sign Me In</p>
-        </div>
+        {this.state.loggedIn ? (
+          <p>{this.state.username}</p>
+        ) : this.state.showPopup ? (
+          <form>
+            <input
+              type="text"
+              value={this.state.username}
+              placeholder="Username"
+              onChange={event =>
+                this.setState({ username: event.target.value })
+              }
+            />
+            <input
+              type="password"
+              value={this.state.password}
+              placeholder="Password"
+              onChange={event =>
+                this.setState({ password: event.target.value })
+              }
+            />
+            <button type="submit" value="Submit" onClick={this.onSubmit}>Submit</button>
+          </form>
+        ) : (
+          <div className="signin-link" onClick={this.toggleSignInPopup}>
+            <p className="sign-in-text">Sign Me In</p>
+          </div>
+        )}
       </>
     );
   }
